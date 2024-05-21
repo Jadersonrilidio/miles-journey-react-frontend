19537f16df2a66d5dc2bf5419689c7adb1dceda2
jay <jadersonrilidio@gmail.com>
2024-05-21 11:19:05 -0300

    modified API URL on globals.ts

--- globals.ts
+++ globals.ts
@@ -1,4 +1,3 @@
 export const REACT_APP_STORAGE_ACCESS_TOKEN_KEY: string = 'access_token';
-
-export const REACT_APP_BASE_URL: string = 'http://localhost:8000';
+export const REACT_APP_BASE_URL: string = 'https://v5dllt2zyalujgrp34my74h7ou0aixff.lambda-url.sa-east-1.on.aws';
 export const REACT_APP_API_URL: string = REACT_APP_BASE_URL + '/api';
